536deeae699d9dbd5a77f23b00478cd69e3e50cb
phodcharaphon <kikie.potcharapon@gmail.com>
2025-11-29 22:11:16 +0700

    ปรับปรุง server.js: แก้ไข routing, แก้บั๊ก POST, ลบโค้ดไม่ใช้

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use(cors());
